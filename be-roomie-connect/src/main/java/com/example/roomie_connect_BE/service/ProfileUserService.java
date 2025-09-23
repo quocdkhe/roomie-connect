4f5318964fc4000099f1d029bb790785ca08bf0c
lnam22871 <lnam22871@gmail.com>
2025-09-23 11:09:56 +0700

    refactor

--- be-roomie-connect/src/main/java/com/example/roomie_connect_BE/service/ProfileUserService.java
+++ be-roomie-connect/src/main/java/com/example/roomie_connect_BE/service/ProfileUserService.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 public interface ProfileUserService {
 
-    ProfileUserResponse createProfileUser(ProfileUserRequest profileUserRequest);
 
     ProfileUserResponse getProfileUserByUserId();
 
